8934e06f9d11ea398fa0ea75d1485a9072d7d219
Ajaymonish <ajaymonishmurugesan@gmail.com>
2025-12-19 12:31:13 +0530

    changes in database

--- app/common/database_conn.py
+++ app/common/database_conn.py
@@ -1,9 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
+import os
 
-DATABASE_URL = "sqlite:///./hr.db"
+DATABASE_URL = os.getenv("DATABASE_URL")  # Supabase Postgres URL
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
+
 session = sessionmaker(bind=engine, autoflush=False)
 
 Base = declarative_base()    
