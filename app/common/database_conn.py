9791e959e4fef535a52d924bab926c5dcbd3bd1b
Ajaymonish <ajaymonishmurugesan@gmail.com>
2025-12-19 11:44:56 +0530

    changes in database

--- app/common/database_conn.py
+++ app/common/database_conn.py
@@ -1,23 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
-import os
-from dotenv import load_dotenv
-load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL")
 
-
-print(os.getenv("DATABASE_URL"))
-engine = create_engine(
-    DATABASE_URL,
-    pool_pre_ping=True,
-    pool_size=5,        # reduce for Supabase
-    max_overflow=2,
-    connect_args={"sslmode": "require"},
-)
-
-if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL not set in environment")
-
+DATABASE_URL = "sqlite:///./hr.db"
 
 engine = create_engine(DATABASE_URL)
 session = sessionmaker(bind=engine, autoflush=False)
